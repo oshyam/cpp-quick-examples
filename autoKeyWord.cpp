1cea94361b9db2a1001509f5ef1fe04b8339c53e
Shyam Mohan Kunwar <53939662+oshyam@users.noreply.github.com>
2021-02-21 12:42:31 +0530

    added two files

---
+++ autoKeyWord.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+auto num = 108;
+auto add = "Shree Ram Janm Bhumi";
+cout<<num<<endl;
+cout<<add<<endl;
+
+}
---
+++ gcdInbulitFunction.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+#include<algorithm> //__gcd function is hidden in algorithm header preprocessor
+using namespace std;
+int main(){
+cout<<"GCD of (6,20) = "<<__gcd(6,20)<<endl;
+}
